69daa0ffb28ff8c2ae9076c345b613840786a708
Max <iammax64@gmail.com>
2023-01-06 15:52:27 -0500

    Updated a few matrix math functions to correctly use the int vs float routines

--- nmf/main.c
+++ nmf/main.c
@@ -2,22 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
-
-// v ~= w*h
+#include <time.h>
+#include <blas.h>
 
 void main(){
+    srand(time(NULL)); 
     int numrows = 3; //p
     int numcols = 5; //n
     int numtopics = 10; //r
     // double* mat = malloc(numrows*numcols*sizeof(double));
     double* v = malloc(numrows*numcols*sizeof(double)); //p*n
-    double* w = malloc(numrows*numtopics*sizeof(double)); // p*r
-    double* h = malloc(numtopics*numcols*sizeof(double)); // r*n
+    int* w = malloc(numrows*numtopics*sizeof(double)); // p*r
+    int* h = malloc(numtopics*numcols*sizeof(double)); // r*n
+    
+    random_int_mat_init(w, numrows, numtopics);
+    random_int_mat_init(h, numtopics, numcols);
 
     printf ("v: \n");
-    print_mat(v, numrows, numcols);
+    print_double_mat(v, numrows, numcols);
     printf ("w: \n");
-    print_mat(w, numrows, numtopics);
+    print_int_mat(w, numrows, numtopics);
     printf ("h: \n");
-    print_mat(h, numtopics, numcols);
+    print_int_mat(h, numtopics, numcols);
 }
