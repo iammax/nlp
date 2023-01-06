f694ea1d4021509c02d0a5770124fdaf91cd1a79
Max <iammax64@gmail.com>
2023-01-06 14:18:48 -0500

    Added a random initializer for matrices + split the matrix printer into an int and a double version

--- nmf/functions.h
+++ nmf/functions.h
@@ -2,11 +2,28 @@ int mat_index(int i, int j, int numcols){
     return j + i*numcols;
 }
 
-void print_mat(double* mat, int numrows, int numcols){
+void print_int_mat(int* mat, int numrows, int numcols){
+    for (int i = 0; i < numrows; i++){
+        for (int j = 0; j < numcols; j++){
+            printf("%d ", mat[mat_index(i, j, numcols)]);
+        }
+        printf("\n");
+    }
+}
+
+void print_double_mat(double* mat, int numrows, int numcols){
     for (int i = 0; i < numrows; i++){
         for (int j = 0; j < numcols; j++){
             printf("%f ", mat[mat_index(i, j, numcols)]);
         }
         printf("\n");
+    }
+}
+
+void random_int_mat_init(int* mat, int numrows, int numcols){
+    for (int i = 0; i < numrows; i++){
+        for (int j = 0; j < numcols; j++){
+            mat[mat_index(i, j, numcols)] = rand()%10;
+        }
     }
 }
